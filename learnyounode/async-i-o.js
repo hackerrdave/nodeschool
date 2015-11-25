cd2d388ef96ad7853fe9cc59e875c84ff5b17768
Dave Kerr <davek09@gmail.com>
2015-11-24 22:01:36 -0500

    Revert "finish all problems for learnyounode module"
    This reverts commit 1794692faf3a5b4d7f5f3e29409726c58c26999f.

--- learnyounode/async-i-o.js
+++
@@ -1,5 +0,0 @@
-var inputFile = process.argv[2];
-
-require('fs').readFile(inputFile, 'utf-8', function(err, data) {
-  console.log(data.split('\n').length - 1);
-});
--- learnyounode/first-i-o.js
+++
@@ -1,5 +0,0 @@
-var inputFile = process.argv[2];
-var inputString = require('fs').readFileSync(inputFile, 'utf-8');
-var newLineCount = inputString.split('\n').length - 1;
-console.log(newLineCount);
-
