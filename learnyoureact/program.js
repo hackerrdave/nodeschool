eea6409b34f037bb8a29f426c1a20b9acb7df021
Dave Kerr <davek09@gmail.com>
2016-01-23 18:48:33 -0500

    props from server and isomorphic

--- learnyoureact/program.js
+++ learnyoureact/program.js
@@ -1,3 +1,14 @@
+var React          = require('react');
+var ReactDOMServer = require('react-dom/server');
+
+var DOM    = React.DOM;
+var body   = DOM.body;
+var div    = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+var babelify   = require('babelify');
+
 var express = require('express');
 var app = express();
 
@@ -10,9 +21,46 @@ require('babel/register')({
   ignore: false
 });
 
-app.use('/', function(req, res) {
-  res.render('index', '');
+var TodoBox = require('./views/index.jsx');
+
+var data = [
+  { key: '1', title: 'Shopping', detail: process.argv[3] },
+  { key: '2', title: 'Hair cut', detail: process.argv[4] }
+];
+
+app.use('/bundle.js', function(req, res) {
+  res.setHeader('content-type', 'application/javascript');
+
+  browserify({ debug: true })
+    .transform(babelify.configure({
+      presets: ['react', 'es2015'],
+      ignore: ['**/lib/**', '**/node_modules/**'],
+      compact: true
+    }))
+    .require('./app.js', { entry: true })
+    .bundle()
+    .pipe(res);
 });
+
+app.use('/', function(req, res) {
+  var initialData = JSON.stringify(data);
+  var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+  res.setHeader('Content-Type', 'text/html');
+
+  var html = ReactDOMServer.renderToStaticMarkup(body(null,
+    div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+    script({
+      id: 'initial-data',
+      type: 'text/plain',
+      'data-json': initialData
+    }),
+    script({src: '/bundle.js'})
+  ));
+
+  res.end(html);
+});
+
 
 app.listen(app.get('port'), function() {});
 
